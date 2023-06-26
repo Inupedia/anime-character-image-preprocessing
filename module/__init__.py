f83725dafe3540c7904d76d031d90f14026adfe6
Pengju Zhang <xenthorpis2016@gmail.com>
2023-06-25 20:33:30 -0600

    add Real-ESRGAN based image scaler

--- module/__init__.py
+++ module/__init__.py
@@ -8,4 +8,5 @@ from .image_processor import ImageProcessor
 from .image_renamer import ImageRenamer
 from .image_crawler import ImageCrawler
 from .image_cropper import ImageCropper
+from .image_scaler import ImageScaler
 from .config import *
---
+++ module/image_scaler/model/README.md
@@ -0,0 +1,5 @@
+# Pretrained Models
+基础模型: 
+https://huggingface.co/inupedia/Real-ESRGAN
+
+s
