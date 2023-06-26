4b3d222c18316b5527705308881474f6f3940286
Pengju Zhang <xenthorpis2016@gmail.com>
2023-06-26 14:11:43 -0600

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -8,5 +8,6 @@ from .image_processor import ImageProcessor
 from .image_renamer import ImageRenamer
 from .image_crawler import ImageCrawler
 from .image_cropper import ImageCropper
-from .image_scaler import ImageScaler
+
+# from .image_scaler import ImageScaler
 from .config import *
